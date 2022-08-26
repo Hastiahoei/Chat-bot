274b42ec686b15532671600cef853fc0417f4d49
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-08-26 15:27:43 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Chat from './components/Chat';
+import DogPicture from './components/DogPicture';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
